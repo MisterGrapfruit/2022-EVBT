aad033b446c491e72258bf92eaab0406a95d2e35
Ian MacPhail <ianamacphail@hotmail.com>
2022-02-11 16:30:12 -0600

    added armHandler w/out pid, not tested yet, not complete

--- src/main/java/frc/robot/commands/ArmHandler.java
+++ src/main/java/frc/robot/commands/ArmHandler.java
@@ -4,62 +4,42 @@
 
 package frc.robot.commands;
 
-import com.revrobotics.CANSparkMax.IdleMode;
-import com.revrobotics.RelativeEncoder;
-
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.Constants;
 import frc.robot.subsystems.Arm;
 
 public class ArmHandler extends CommandBase {
+  /** Creates a new ArmHandler. */
   private Arm m_arm;
-  private RelativeEncoder armEncoder;
   private JoystickButton downButton;
   private JoystickButton upButton;
-  double downPos = Constants.armDownPosition;
-  double upPos = Constants.armUpPosition;
-  boolean armIsUp;
+  final double armHoldUp = 0.08;
+  final double armHoldDown = 0.13;
+  final double armTravel = 0.5;
 
-  /** Creates a new ArmHandler. */
+  final double armTimeUp = 0.5;
+  final double armTimeDown = 0.35;
+
+
   public ArmHandler(Arm sub, JoystickButton downButton, JoystickButton upButton) {
     // Use addRequirements() here to declare subsystem dependencies.
     this.downButton = downButton;
     this.upButton = upButton;
     m_arm = sub;
-    this.armEncoder = m_arm.armEncoder;
-    addRequirements(sub);
+    addRequirements(m_arm);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {
-    armEncoder.setPosition(downPos);
-  }
+  public void initialize() {}
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {
-    if(downButton.get() && armIsUp==true){
-      armIsUp = false;
-      m_arm.setPosition(downPos);
-    }
-    if( Math.abs(armEncoder.getPosition()-(downPos)) < 0.01) {
-      m_arm.armStop();
-      m_arm.setBrake(IdleMode.kCoast);
-    }
-    if(upButton.get() && armIsUp==false){
-      m_arm.setBrake(IdleMode.kBrake);
-      armIsUp = true;
-      m_arm.setPosition(upPos);
-    }
-  }
+  public void execute() {}
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {
-    m_arm.setBrake(IdleMode.kBrake);
-  }
+  public void end(boolean interrupted) {}
 
   // Returns true when the command should end.
   @Override
