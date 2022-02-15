3b43a6d9381734fc6a32acce794add3f992bbb65
Ian MacPhail <ianamacphail@hotmail.com>
2022-02-15 17:07:22 -0600

    rolling back a bit

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -30,6 +30,41 @@ public class Drivetrain extends SubsystemBase {
     rightFront.set(rightSpeed);
   }
 
+  public void arcadeDrive(double throttle, double twist){
+    if(Math.abs(throttle) < 0.1){
+      throttle = 0;
+    }
+    if(Math.abs(twist) < 0.1){
+      twist = 0;
+    }
+    /*
+    Arcade Input Scaling - WIP
+
+    double sInput;
+    double lesserInput = Math.min(Math.abs(throttle), Math.abs(twist));
+    double greaterInput = Math.max(Math.abs(throttle), Math.abs(twist));
+    if(greaterInput > 0){
+      sInput = lesserInput/greaterInput + 1;
+    }
+    else{
+      sInput = 1;
+    }
+    throttle /= sInput;
+    twist /= sInput;
+    */
+    //throttle *= Math.abs(throttle) * 0.8;
+    //twist *= Math.abs(twist) * 0.6;
+    throttle*=throttle*=throttle;
+    twist*=twist*=twist*Math.abs(twist);
+    setSpeed(twist+throttle, -twist+throttle);
+    System.out.print("Throttle = " + throttle);
+    System.out.println("  |  Twist = " + twist);
+    System.out.print(leftFront.get());
+    System.out.print(" | " + leftBack.get());
+    System.out.print(" | " + rightFront.get());
+    System.out.println(" | " + rightBack.get());
+  }
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
